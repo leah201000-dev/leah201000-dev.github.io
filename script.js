8c869cbfdd181a575c5227edb850791d2c06e135
leah201000-dev <leah201000@gmail.com>
2025-09-09 17:23:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const buttons = document.querySelectorAll('nav button');
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const targetId = button.getAttribute('data-target');
-    const section = document.getElementById(targetId);
-    section.scrollIntoView({ behavior: 'smooth' });
+    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
   });
 });
